2570c1267132261a9ecc43a31c58fef31f31463f
yuhan676 <yuhanhu676@gmail.com>
2024-06-20 15:51:42 +0100

    Fixes issues with the weather panel component and smog allert
    Fixes #17
    Fixes #16

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import MapComponent from './components/MapComponent/MapComponent.js';
 import Sidebar from './components/Sidebar/Sidebar.js';
+import SmogAlert from './components/SmogAlert/SmogAlert.js';
+import WeatherPanel from './components/WeatherPanel/WeatherPanel.js';
 
 
 function App() {
   return (
     <div className="App">
+      <SmogAlert />
       <Sidebar />
       <MapComponent />
+      <WeatherPanel />
     </div>
   );
 }
