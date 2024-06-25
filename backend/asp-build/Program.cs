9498bc1d52a4ed4132cccd3c8fbd82b7dfab8efa
Adam Southwick <adamsouthwick100@gmail.com>
2024-06-25 12:32:40 +0100

    seperated backend into 2 projects

--- backend/asp-build/Program.cs
+++ backend/asp-build/Program.cs
@@ -1,16 +1,13 @@
-using backend.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.Configure<Neo4jOptions>(builder.Configuration.GetSection("Neo4j"));
-builder.Services.AddSingleton<Neo4jService>();
-
 var app = builder.Build();
 
-// Configure the Dev http test pipeline with Swagger
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
