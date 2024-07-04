8633760d76bbc9f5435f41a5bece1d19bb7b1d5b
Adam Southwick <adamsouthwick100@gmail.com>
2024-07-04 17:37:12 +0100

    new .envs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -143,19 +143,26 @@ function App() {
   }
 
   async function fetchWeatherData() {
-    const apiUrl = 'http://localhost:5000/api/weather';
+    await blind_test('http://localhost:5000/api/weather', "1");
+    await blind_test('http://asp-run/api/weather', "2");
+    await blind_test('http://asp-run:8080/api/weather', "3");
+    await blind_test('http://asp-run:5000/api/weather', "4");
+  }
+  
+  async function blind_test(url, num) {
+    const apiUrl = url;
     try {
-      const response = await fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        credentials: 'include',
-      });
-      const data = await response.json();
-      console.log(data);
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const weatherData = await response.json();
+      console.log('Weather data:', weatherData);
+      console.log('num:', num);
+      //setWeather(weatherData); 
     } catch (error) {
-      console.error('Error fetching weather data:', error);
+      console.error('Failed to fetch weather data:', error);
+      //setWeather(null); 
     }
   }
 
