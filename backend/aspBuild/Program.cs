eea20d166670e6a6b0acf19034c28ba1aac94bca
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-07-05 09:47:04 +0100

    Updated to add Binary search for Parks

--- backend/aspBuild/Program.cs
+++ backend/aspBuild/Program.cs
@@ -1,4 +1,41 @@
-﻿using aspBuild.Data;
+﻿using System.Diagnostics;
+using aspBuild.Data;
 
 string parkFilePath = "DataFiles\\ParkNodes.txt";
 ParkNodes parkNodes= new ParkNodes(parkFilePath);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Testing efficiency between default List Contains and custom binary search
+        // Random random = new Random();
+        // Stopwatch stopwatch = new Stopwatch();
+        // stopwatch.Start();
+        // foreach (var item in parkNodes.ParkNodeList)
+        // {
+        //     parkNodes.CheckIfPark(item);
+        //     parkNodes.CheckIfPark(random.NextInt64(0,10000));
+        // }
+        // stopwatch.Stop();
+        // Console.WriteLine("Elapsed Time: {0} milliseconds", stopwatch.ElapsedMilliseconds);
+
+
+        // Stopwatch newstopwatch = new Stopwatch();
+        // newstopwatch.Start();
+        // foreach (var item in parkNodes.ParkNodeList)
+        // {
+        //     parkNodes.CheckIfPark(item);
+        //     parkNodes.CheckIfPark(random.NextInt64(0,10000));
+        // }
+        // newstopwatch.Stop();
+        // Console.WriteLine("Elapsed Time: {0} milliseconds", newstopwatch.ElapsedMilliseconds);
