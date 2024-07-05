c6bd9557146a369a340dfdd9b50f13e5b56aa8ad
yuhan676 <yuhanhu676@gmail.com>
2024-07-06 00:16:50 +0100

    Fixes #102 Fixes #34 Fixes #32 Zoom-onto route, display POI clustered markers close to route, display 311s close to route

---
+++ frontend/src/assets/geodata/fetchGarbage311.js
@@ -0,0 +1,134 @@
+function fetchGarbage311() {
+  return [
+  {
+    "coordinates": [
+      -73.94173594930122,
+      40.830000031764946
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Dirty Condition"
+  },
+  {
+    "coordinates": [
+      -73.94882063297837,
+      40.822408765090266
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Dirty Condition, Illegal Dumping"
+  },
+  {
+    "coordinates": [
+      -73.99947683942626,
+      40.72867637267499
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Dirty Condition, Illegal Dumping, Street Sweeping Complaint"
+  },
+  {
+    "coordinates": [
+      -73.940707808814,
+      40.82402975152741
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Dirty Condition"
+  },
+  {
+    "coordinates": [
+      -73.94006492355682,
+      40.823741223896725
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Dirty Condition"
+  },
+  {
+    "coordinates": [
+      -73.99964639015316,
+      40.73347420503014
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Illegal Dumping, Dirty Condition"
+  },
+  {
+    "coordinates": [
+      -73.98930457456102,
+      40.73678662631891
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Dumpster Complaint, Residential Disposal Complaint"
+  },
+  {
+    "coordinates": [
+      -73.92114009182538,
+      40.86871558176139
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Residential Disposal Complaint, Dirty Condition"
+  },
+  {
+    "coordinates": [
+      -73.9950920595976,
+      40.74300113606367
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Dirty Condition, Illegal Dumping"
+  },
+  {
+    "coordinates": [
+      -73.97811575623038,
+      40.744838144086906
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Sidewalk Condition"
+  },
+  {
+    "coordinates": [
+      -73.98599648506749,
+      40.74989794381453
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Residential Disposal Complaint"
+  },
+  {
+    "coordinates": [
+      -74.0012016103309,
+      40.73671849788938
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Commercial Disposal Complaint, Residential Disposal Complaint"
+  },
+  {
+    "coordinates": [
+      -74.00088757339687,
+      40.72900848589757
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Residential Disposal Complaint"
+  },
+  {
+    "coordinates": [
+      -73.95899539382252,
+      40.76327213006063
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Dirty Condition, Missed Collection"
+  },
+  {
+    "coordinates": [
+      -73.95018800672294,
+      40.78644797158429
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Dirty Condition"
+  },
+  {
+    "coordinates": [
+      -74.00154803384405,
+      40.73729214707372
+    ],
+    "category": "High",
+    "Complaint_Type_collection": "Residential Disposal Complaint, Illegal Dumping"
+  }
+];
+}
+
+export default fetchGarbage311;
