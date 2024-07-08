c8598490230cf8b1cdf457d509e7dde3b66336bd
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-07-08 14:13:54 +0100

    deleted comments

--- backend/aspBuild/Program.cs
+++ backend/aspBuild/Program.cs
@@ -2,16 +2,6 @@
 using aspBuild.Data;
 using Neo4j.Driver;
 
-
-// string parkFilePath = "DataFiles\\ParkNodes.txt";
-// ParkNodes parkNodes= new ParkNodes(parkFilePath);
-    
-// string jsonTaxiPath = "DataFiles\\taxi_data_final.json";
-// var jsonDataTaxi = GetJSONs.getJSON(jsonTaxiPath);
-
-// string jsonSubwayPath = "DataFiles\\subway_data_final.json";
-// var jsonDataSubway = GetJSONs.getJSON(jsonSubwayPath);
-
 // Updates and times the update function
 Stopwatch stopwatch = Stopwatch.StartNew();
 var updateDatabase = new UpdateDatabase();
@@ -22,28 +12,3 @@ Console.WriteLine("Elapsed Time: {0} milliseconds", stopwatch.ElapsedMillisecond
 
 Console.WriteLine("Completed.");
 
-
-
-
-// Testing efficiency between default List Contains and binary search
-        // Random random = new Random();
-        // Stopwatch stopwatch = new Stopwatch();
-        // stopwatch.Start();
-        // foreach (var item in parkNodes.ParkNodeList)
-        // {
-        //     parkNodes.CheckIfPark(item);
-        //     parkNodes.CheckIfPark(random.NextInt64(0,10000));
-        // }
-        // stopwatch.Stop();
-        // Console.WriteLine("Elapsed Time: {0} milliseconds", stopwatch.ElapsedMilliseconds);
-
-
-        // Stopwatch newstopwatch = new Stopwatch();
-        // newstopwatch.Start();
-        // foreach (var item in parkNodes.ParkNodeList)
-        // {
-        //     parkNodes.CheckIfPark(item);
-        //     parkNodes.CheckIfPark(random.NextInt64(0,10000));
-        // }
-        // newstopwatch.Stop();
-        // Console.WriteLine("Elapsed Time: {0} milliseconds", newstopwatch.ElapsedMilliseconds);
