70fa0fcda81565d42b1c402cf9a9a21d5df5df04
Adam <65317086+cleffei@users.noreply.github.com>
2024-07-11 15:56:19 +0100

    Colour & map theme for dark mode, static for now

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 const MAPBOX_GEOCODING_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places";
-const MAPBOX_STYLE_URL= "mapbox://styles/yuhanhu676/clyczifiq00mx01qv8l0g2tbo";
+const MAPBOX_STYLE_URL= "mapbox://styles/yuhanhu676/clyh9efj100qm01pf31c47f3x";
 export { MAPBOX_TOKEN, MAPBOX_GEOCODING_URL, MAPBOX_STYLE_URL};
