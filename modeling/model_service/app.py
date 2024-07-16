da9e0aca690d0dc02acd4e67042811bf56ac44e4
Adam Southwick <adamsouthwick100@gmail.com>
2024-07-16 22:33:27 +0100

    cors bug passed locally trying on server

--- modeling/model_service/app.py
+++ modeling/model_service/app.py
@@ -4,11 +4,6 @@ from predict import call_taxi_model, call_subway_model
 
 app = Flask(__name__)
 
-CORS(app, resources={
-    r"/taxi": {"origins": "https://clearpath.info.gf"},
-    r"/subway": {"origins": "https://clearpath.info.gf"}
-})
-
 @app.route('/taxi', methods=['GET'])
 def taxi_api():
     return call_taxi_model()
@@ -18,4 +13,5 @@ def subway_api():
     return call_subway_model()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    CORS(app, origins=["http://asp-build:8080"])
