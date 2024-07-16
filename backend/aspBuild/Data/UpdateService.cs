c57073d883e54e9d4fb5586b1a7858198896c47a
Adam Southwick <adamsouthwick100@gmail.com>
2024-07-16 17:11:42 +0100

    put update on a timer

--- backend/aspBuild/Data/UpdateService.cs
+++ backend/aspBuild/Data/UpdateService.cs
@@ -9,22 +9,38 @@ namespace aspBuild.Data
         private readonly UpdateDatabase _updateDatabase;
         private Timer? _timer;
         private readonly ILogger<UpdateService> _logger;
+        private bool _blockOverlap;
 
         public UpdateService(ILogger<UpdateService> logger, UpdateDatabase updateDatabase)
         {
             _updateDatabase = updateDatabase;
             _timer = null;
             _logger = logger;
+            _blockOverlap = false;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Starting background service...");
-            _timer = new Timer(ExecuteTask, null, TimeSpan.Zero, TimeSpan.FromHours(2));
+            _timer = new Timer(async state => await CheckTime(state), null, TimeSpan.Zero, TimeSpan.FromMinutes(5));
             return Task.CompletedTask;
         }
 
-        private async void ExecuteTask(object? state)
+        private async Task CheckTime(object? state)
+        {
+            if (_blockOverlap) {
+                _blockOverlap = false;
+                return;
+            }
+            var now = DateTime.Now.TimeOfDay;
+            if (now.Minutes < 5 && now.Hours % 2 == 0)
+            {
+                await ExecuteTask();
+                _blockOverlap = true;
+            }
+        }
+
+        private async Task ExecuteTask()
         {
             await _updateDatabase.RunUpdate();
         }
