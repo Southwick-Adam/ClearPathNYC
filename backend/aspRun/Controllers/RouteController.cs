ac39f52a3254efe2158aab013e69cdad5e354b84
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-07-19 14:34:11 +0100

    route for loop

--- backend/aspRun/Controllers/RouteController.cs
+++ backend/aspRun/Controllers/RouteController.cs
@@ -71,6 +71,35 @@ namespace aspRun.Controllers
             var geoJson = _neo4jService.GeoJSON(finalCoordinates.ToString(), "P2P", "false", "[]", finalCosts.ToString());
             return Ok(geoJson);
         }
+
+
+        
+        // TODO: Fix routes
+        [HttpGet("loop")]
+        public async Task<IActionResult> Loop([FromQuery] List<double> coordinate, [FromQuery] double distance, [FromQuery] bool quiet)
+        {
+            Console.WriteLine($"Coordinates: {coordinate}");
+            
+            if (coordinate == null || coordinate.Count < 2)
+            {
+                return BadRequest("Coordinates must contain at least two elements.");
+            }
+
+            double longitude = coordinate[0];
+            double latitude = coordinate[1];
+            string result;
+
+            try
+            {
+                result = await _neo4jService.Loop(latitude, longitude, distance, quiet);                
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while connecting to the Neo4j database: {ex.Message}");
+            }
+
+            return Ok(result);
+        }
     }
 }
 
