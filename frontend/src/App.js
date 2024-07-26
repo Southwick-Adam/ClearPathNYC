400f6906c52fee62ccff767c61dcd32607d58513
Adam Southwick <adamsouthwick100@gmail.com>
2024-07-26 17:49:10 +0100

    pointers at deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -106,7 +106,7 @@ function App() {
     });
     params.append('quiet', isQuiet); // Add the quiet parameter
   
-    const requestUrl = `http://localhost:5056/route/p2p?${params.toString()}`;
+    const requestUrl = `/route/p2p?${params.toString()}`;
     console.log('Request URL:', requestUrl);
   
     try {
@@ -135,7 +135,7 @@ function App() {
     });
     params.append('quiet', isQuiet); // Add the quiet parameter
   
-    const requestUrl = `http://localhost:5056/route/multip2p?${params.toString()}`;
+    const requestUrl = `/route/multip2p?${params.toString()}`;
     console.log('Request URL:', requestUrl);
   
     try {
@@ -164,7 +164,7 @@ function App() {
     params.append('distance', distanceMeter);
     params.append('quiet', mode);
   
-    const requestUrl = `http://localhost:5056/route/loop?${params.toString()}`;
+    const requestUrl = `/route/loop?${params.toString()}`;
     console.log('Request URL:', requestUrl);
   
     try {
@@ -184,7 +184,7 @@ function App() {
   }
 
   async function fetchWeatherData() {
-    const apiUrl = 'http://localhost:5056/weather';
+    const apiUrl = '/weather';
     try {
       const response = await fetch(apiUrl, {
         method: 'GET',
